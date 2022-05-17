b335e95cc2c9deacf846f65765ce0ca105ac8590
srubio <srubio@cells.es>
2022-05-17 10:11:14 +0200

    folder reestructured

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 
 DS = 'VacuumController'
 description = '%s Tango Device Server'%DS
-version = open('VERSION').read().strip()
-package = DS
+version = open(DS+'/VERSION').read().strip()
+license = 'GPL-3.0'
 
 __doc__ = """
 Generic Device Server setup.py file copied from fandango/scripts/setup.ds.py
@@ -33,25 +33,18 @@ To tune some options:
 -------------------------------------------------------------------------------
 """
 
-#print(__doc__)
-
-license = 'GPL-3.0'
-install_requires = ['fandango',
-                    'PyTango',]
-
 ## All the following defines are OPTIONAL
+install_requires = ['fandango','PyTango',]
 
 ## For setup.py located in root folder or submodules
 package_dir = {
-    DS: '.',
-    #'DS/tools': './tools',
+    DS: DS,    #'DS/tools': './tools',
 }
 packages = package_dir.keys()
 
 ## Additional files, remember to edit MANIFEST.in to include them in sdist
 package_data = {'': [
-  'VERSION',
-  'CHANGES',
+  DS+'/VERSION',
   #'./tools/icon/*',
   #'./tools/*ui',
 ]}
@@ -59,7 +52,7 @@ package_data = {'': [
 ## Launcher scripts
 scripts = [
   #DS,
-  #'./bin/'+DS,
+  './bin/'+DS,
   #'./scripts/VacuumController',
   #'./scripts/MVC3GaugeController',
   #'./scripts/PfeifferGaugeController',
@@ -69,22 +62,24 @@ scripts = [
 
 ## This option relays on DS.py having a main() method
 entry_points = {
-        'console_scripts': [
-            '%s = %s.%s:main'%(DS,DS,DS),
-        ],
+#        'console_scripts': [
+#            '%s = %s.%s:main'%(DS,DS,DS),
+#        ],
 }
 
 
 setup(
-    name=package,
-    version=version,
-    license=license,
-    description=description,
-    install_requires=install_requires,    
+    name = DS.lower(),
+    author = 'srubio',
+    author_email = 'controls-software@cells.es',
+    version = version,
+    license = license,
+    description = description,
+    install_requires = install_requires,    
     packages = packages or find_packages(),
-    package_dir= package_dir,
-    entry_points=entry_points,    
+    package_dir = package_dir,
+    entry_points = entry_points,    
     scripts = scripts,
     include_package_data = True,
-    package_data = package_data    
+    package_data = package_data,
 )
